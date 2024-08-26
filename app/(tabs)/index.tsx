1b9c0e1b31e34d7b09327854fc0f5282da918274
Obaino Chris <obainochrisy@gmail.com>
2024-08-26 01:51:52 +0100

    commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,25 @@
-import { StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Link } from 'expo-router';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-
-export default function TabOneScreen() {
+const Odance = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text>Odance</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: 'blue'}}>Go To Profile</Link>
     </View>
   );
 }
 
+export default Odance
+
 const styles = StyleSheet.create({
-  container: {
+  container:{
+    display: 'flex',
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+    justifyContent: 'center'
+  }
+})
